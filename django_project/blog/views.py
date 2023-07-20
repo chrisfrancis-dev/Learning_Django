e57fb094375347befa280c8869aca0f5159a90e8
Chris Francis <chrisfrancis.dev@gmail.com>
2023-07-20 08:45:30 +0530

    adding templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# Since we are not using HttpResponse anymore I commented it out
+# from django.http import HttpResponse # added import statement
 # Create your views here.
+
+post = [
+    {
+        'author':'Chris Francis',
+        'topic':'HTML Basics',
+        'description':'This is the Basics',
+        'date':'July-20'
+    },
+    {
+        'author':'Chris Francis',
+        'topic':'HTML Basics',
+        'description':'This is the Basics',
+        'date':'July-20'
+    }
+] 
+
 def home(request):
-    return HttpResponse('<h1>BLOG PAGE<h1>')
+    context = {
+        'posts':post
+    }
+    # return HttpResponse('<h1>HOME PAGE<h1>') commented since we don't need it 
+    # request, then the template where the html page is located and then context which is data which will be manipulated on the html page
+    return render(request,'blog/home.html',context) 
 
 def about(request):
-    return HttpResponse('<h1>BLOG ABOUT<h1>')
+    # return HttpResponse('<h1>BLOG ABOUT<h1>') commented since we don't need it
+    return render(request,'blog/about.html',{'title': 'about'})
