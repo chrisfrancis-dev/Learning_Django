45a8c29392eacae41dcba9a9cc6908c3617f8897
Chris Francis <chrisfrancis.dev@gmail.com>
2023-07-24 15:55:34 +0530

    adding data to the database and using that in the project, also added the Post model to tthe admin portal

--- django_project/blog/admin.py
+++ django_project/blog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Post # importing the post model 
 # Register your models here.
+admin.site.register(Post) # this is to get the post model in the admin portal
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,26 +1,30 @@
 from django.shortcuts import render
+# since models is in the same directory we use .models
+from .models import Post 
 # Since we are not using HttpResponse anymore I commented it out
 # from django.http import HttpResponse # added import statement
 # Create your views here.
-
-post = [
-    {
-        'author':'Chris Francis',
-        'title':'HTML Basics',
-        'content':'This is the Basics',
-        'date_posted':'July-20'
-    },
-    {
-        'author':'Chris Francis',
-        'title':'HTML Basics',
-        'content':'This is the Basics',
-        'date_posted':'July-20'
-    }
-] 
+# this dummy data is no longer needed as our data will be comming from the database
+# post = [
+#     {
+#         'author':'Chris Francis',
+#         'title':'HTML Basics',
+#         'content':'This is the Basics',
+#         'date_posted':'July-20'
+#     },
+#     {
+#         'author':'Chris Francis',
+#         'title':'HTML Basics',
+#         'content':'This is the Basics',
+#         'date_posted':'July-20'
+#     }
+# ] 
 
 def home(request):
     context = {
-        'posts':post
+        # Getting our post data from the database
+        'posts' : Post.objects.all()
+        # 'posts':post
     }
     # return HttpResponse('<h1>HOME PAGE<h1>') commented since we don't need it 
     # request, then the template where the html page is located and then context which is data which will be manipulated on the html page it has to be a dictionary
