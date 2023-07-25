b0737fc007aa3c719d5ae0108c75cbca2c2825c3
Chris Francis <chrisfrancis.dev@gmail.com>
2023-07-25 16:33:13 +0530

    adding an email field to the form and adding crispy formatting

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,9 +1,12 @@
 # addin the redirect method to the import statement to change the page once the registration has been done 
 from django.shortcuts import render, redirect 
 # django already has a form template for us so that we don't have to worry about user authentication etc, we are importing that form
-from django.contrib.auth.forms import UserCreationForm 
+# from django.contrib.auth.forms import UserCreationForm # commenting this out because we will be using our new created form
 # imports the packages to show a flash message
 from django.contrib import messages
+# importing the form that we made in forms.py this has an extra email field
+# so now we have to replace UserCreationForm with UserRegistrationForm
+from .forms import UserRegistrationForm
 # Create your views here.
 
 def register(request):
@@ -11,7 +14,7 @@ def register(request):
     # checks if the request is POST, if it is then data is comming from the form
     if request.method == 'POST':
         # this renders a form with the data that was inputed by the user
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         # checking if the form data is valid
         if form.is_valid():
             # will save the user in the database
@@ -23,5 +26,5 @@ def register(request):
             return redirect('blog-home') # this was the name given to the home page in the urls.py of the blog app
     else:
         # rendering a blank form 
-        form = form = UserCreationForm()     
+        form = form = UserRegistrationForm()     
     return render(request, 'users/register.html', {'form':form}) # passed users as a parameter
